3bd1f06e811e7b239de9272fb33fae993d095a31
ATFfang <643737159@qq.com>
2026-01-06 14:24:09 +0800

    first commit

---
+++ georag-backend/src/main/java/com/EarthCube/georag_backend/GeoRagApplication.java
@@ -0,0 +1,13 @@
+package com.EarthCube.georag_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoRagApplication.class, args);
+	}
+
+}
---
+++ georag-backend/src/test/java/com/EarthCube/georag_backend/GeoRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EarthCube.georag_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
